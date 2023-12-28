bda9583561b3cb444ac4707573d11d3fe765af3b
Anthony M. Bonafide <anthony.bonafide@shipt.com>
2023-12-28 10:08:40 -0500

    remove git fugitive
    Signed-off-by: Anthony M. Bonafide <anthony.bonafide@shipt.com>

--- lua/anthonybonafide/packer.lua
+++ lua/anthonybonafide/packer.lua
@@ -38,7 +38,6 @@ return require('packer').startup(function(use)
     use {'nvim-treesitter/playground'}
     use {'theprimeagen/harpoon'}
     use {'mbbill/undotree'}
-    use {'tpope/vim-fugitive'}
 
     use {
 	    'VonHeikemen/lsp-zero.nvim',
